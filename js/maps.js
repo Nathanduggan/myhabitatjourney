6414e2e1c27a24a1e2d8e0bd22b2d543e25d0fec
Nathan Duggan <nathan.duggan0@gmail.com>
2019-02-13 22:07:31 +0000

    Updating Google Maps API

--- js/maps.js
+++ js/maps.js
@@ -2,6 +2,7 @@
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 3,
           center: {lat: 54.5260, lng: 15.2551},
+          center: {lat: 8.7832, lng: 34.5085},
           mapTypeId: 'terrain'
         });
         
@@ -10,7 +11,6 @@
 
         var flightPlanCoordinates = [
           {lat: 53.3498, lng: -6.2603},
-         
           {lat: -16.0252, lng: 35.5083}
         ];
          var flightPath = new google.maps.Polyline({
