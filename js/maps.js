7c4268d0317d212922817ad082cdda3c47c87fcb
Nathan Duggan <nathan.duggan0@gmail.com>
2019-04-03 21:19:41 +0000

    Updated Maps, Nav, Social Links and Email

--- js/maps.js
+++ js/maps.js
@@ -1,26 +1,10 @@
-	  function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 3,
-          center: {lat: 54.5260, lng: 15.2551},
-          center: {lat: 8.7832, lng: 34.5085},
-          mapTypeId: 'terrain'
-        });
-        
-      
-        
-
-        var flightPlanCoordinates = [
-          {lat: 53.3498, lng: -6.2603},
-          {lat: -16.0252, lng: 35.5083}
-        ];
-         var flightPath = new google.maps.Polyline({
-          path: flightPlanCoordinates,
-          geodesic: true,
-          strokeColor: '#FF5733',
-          strokeOpacity: 2.0,
-          strokeWeight: 1
-        });
-
-        flightPath.setMap(map);
-      }
-        
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 53.1424, lng: -7.6921};
+   var uluru = {lat: -13.2543, lng: 34.3015};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
--- js/myscript.js
+++ js/myscript.js
@@ -1,72 +1,58 @@
-$(".button1").click(function() {
-        $(".test1").slideToggle('slow');
-        
-   });
-      
-    $(".button2").click(function() {
-        $(".test2").slideToggle('1000');
-        
-       
-    }); 
-    
-   
- $(".btn-primary").click(function(){
-  alert("Webpage under maintenance.");
+$(document).ready(function(){
+    /*----$(".fa-facebook").hover(function(){
+    $(this).css("font-size", "44px");
+    }, function(){
+    $(this).css("font-size", "22px");
+  });
 });
 
-$(".button1").on({
-    mouseenter: function() {
-        $(this).css("background-color", "#");
-    },
-    mouseleave: function() {
-    $(this).css("background-color", "#");
-},
-click: function() {
-    $(this).css("background-color", "#");
-    }
-    });
-    
-    $(".button2").on({
-    mouseenter: function() {
-        $(this).css("background-color", "#");
-    },
-    mouseleave: function() {
-    $(this).css("background-color", "#");
-},
-click: function() {
-    $(this).css("background-color", "#");
-    }
-    });
-    
-    $(document).ready(function(){
-     $(".panel").click(function(){
-    $("#panel").slideToggle("slow");
+$(".fa-twitter").hover(function(){
+    $(this).css("font-size", "44px");
+    }, function(){
+    $(this).css("font-size", "22px");
   });
+  
+  $(".fa-youtube").hover(function(){
+    $(this).css("font-size", "44px");
+    }, function(){
+    $(this).css("font-size", "22px");
+  });
+  
+  $(".fa-google").hover(function(){
+    $(this).css("font-size", "44px");
+    }, function(){
+    $(this).css("font-size", "22px");
+  });
+  */
+
+$(document).ready(function() {
+    $(".panel").click(function() {
+        $("#panel").slideToggle("slow");
     });
-    
-      $(".panel2").click(function(){
+});
+
+$(".panel2").click(function() {
     $("#panel2").slideToggle("slow");
-  });
-  
-     $(".panel3").click(function(){
+});
+
+$(".panel3").click(function() {
     $("#panel3").slideToggle("slow");
-  });
-  
-     $(".panel4").click(function(){
+});
+
+$(".panel4").click(function() {
     $("#panel4").slideToggle("slow");
-  });
+});
 
-   $(".panel5").click(function(){
+$(".panel5").click(function() {
     $("#panel5").slideToggle("slow");
-  });
+});
 
-   $(".panel6").click(function(){
+$(".panel6").click(function() {
     $("#panel6").slideToggle("slow");
-  });
+});
 
-   $(".panel7").click(function(){
+$(".panel7").click(function() {
     $("#panel7").slideToggle("slow");
-  });
-
+});
 
 
